cc7c68ebf9f2a197d6b4034994cee8aa1a4eb107
Thomas Geirhovd <thomas@linted.net>
2015-04-10 12:44:27 +0200

    Some dotfiles and a setup script

---
+++ .vimrc
@@ -0,0 +1,38 @@
+set nocompatible              " be iMproved, required
+filetype off                  " required
+ 
+" set the runtime path to include Vundle and initialize
+set rtp+=~/.vim/bundle/Vundle.vim
+call vundle#begin()
+" alternatively, pass a path where Vundle should install plugins
+"call vundle#begin('~/some/path/here')
+ 
+" let Vundle manage Vundle, required
+Plugin 'gmarik/Vundle.vim'
+ 
+" The following are examples of different formats supported.
+" Keep Plugin commands between vundle#begin/end.
+Plugin 'tpope/vim-rails' 
+" All of your Plugins must be added before the following line
+call vundle#end()            " required
+filetype plugin on
+syntax on
+set tabstop=2
+set shiftwidth=2
+set expandtab
+colo distinguished
+let mapleader = ','
+set nu
+set ruler
+set cc=80
+
+" Append modeline after last line in buffer.
+" Use substitute() instead of printf() to handle '%%s' modeline in LaTeX
+" files.
+function! AppendModeline()
+  let l:modeline = printf(" vim: set ts=%d sw=%d tw=%d %set :",
+        \ &tabstop, &shiftwidth, &textwidth, &expandtab ? '' : 'no')
+  let l:modeline = substitute(&commentstring, "%s", l:modeline, "")
+  call append(line("$"), l:modeline)
+endfunction
+nnoremap <silent> <Leader>ml :call AppendModeline()<CR>
---
+++ setup.sh
@@ -0,0 +1,87 @@
+#!/usr/bin/env zsh
+set -x
+unset dotdir
+
+# Init
+# Get script directory
+dotdir=$(dirname ${0:A})
+# Detect OS
+os="dunno"
+unamestr=$(uname)
+if [[ "$unamestr" == "Linux" ]]; then
+  distrostr=$(lsb_release -si)
+  if [[ "$distrostr" =~ /RedHat/ ]]; then
+    os="redhat"
+  elif [[ "$distrostr" =~ /Fedora/ ]]; then
+    os="redhat"
+  else
+    os=$distrostr
+  fi
+elif [[ "$unamestr" == "Darwin" ]]; then
+  os=$unamestr
+  if ! type "brew" > /dev/null; then
+    # Install homebrew
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  fi
+else
+  os=$unamestr
+fi
+
+# Make vim awesome
+vim_conf() {
+  # Copy .vimrc
+  mv ~/.vimrc ~/.vimrc.lame
+  cp $dotdir/.vimrc ~/.vimrc
+  # Install vundle
+  mkdir -p ~/.vim/bundle
+  git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+  vim +PluginInstall +qall
+  # Install distinguished color theme
+  mkdir -p ~/.vim/colors
+  curl https://raw.github.com/Lokaltog/vim-distinguished/develop/colors/distinguished.vim > ~/.vim/colors/distinguished.vim
+}
+
+# Make tmux awesome
+tmux_conf() {
+  # Check if tmux is installed, and install if not
+  if ! type "tmux" > /dev/null; then
+    install_pkg tmux
+  fi
+  # Copy .tmux.conf
+  mv ~/.tmux.conf ~/.tmux.conf.lame
+  cp $dotdir/.tmux.conf ~/.tmux.conf
+}
+
+# Make zsh awesome
+zsh_conf() {
+  # Copy .zshrc
+  mv ~/.zshrc ~/.zshrc.lame
+  cp $dotdir/.zshrc ~/.zshrc
+  # Install oh-my-zsh
+  curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+}
+
+# Install rvm and ruby
+rvm() {
+  gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+  \curl -sSL https://get.rvm.io | bash -s stable
+  rvm install 2.2
+  rvm use 2.2 --default
+}
+
+install_pkg() {
+  if [[ "$os" == "Darwin" ]]; then
+    $(brew install $1)
+  elif [[ "$os" == "redhat" ]]; then
+    $(yum install $1)
+  elif [[ "$os" == "Ubuntu" ]]; then
+    $(apt-get install $1)
+  else
+    echo "Unknown OS, cannot install $1"
+  fi
+}
+
+#vim_conf
+#tmux_conf
+#zsh_conf
+#rvm
