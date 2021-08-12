a200e3f7375d9abed7e42e0723f33b6a8387e70c
Thomas Geirhovd <thomas.geirhovd@schibsted.com>
2021-08-12 13:35:47 +0200

    Remove ruby stuff

--- setup.sh
+++ setup.sh
@@ -125,17 +125,8 @@ populate_bin() {
   fi
 }
 
-# Install rvm and ruby
-rvm() {
-  gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-  \curl -sSL https://get.rvm.io | bash -s stable
-  rvm install 2.2
-  rvm use 2.2 --default
-}
-
 install_dotfiles
 vim_conf
 tmux_conf
 zsh_conf
 populate_bin
-#rvm
