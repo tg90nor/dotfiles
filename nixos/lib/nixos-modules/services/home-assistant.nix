a5679f98cb98a3ba8dd440e0381ac859d48bb2ef
Thomas Geirhovd <tg90nor@gmail.com>
2026-04-20 22:27:54 +0200

    home-assistant: run container as root to avoid permission issues

--- nixos/lib/nixos-modules/services/home-assistant.nix
+++ nixos/lib/nixos-modules/services/home-assistant.nix
@@ -1,14 +1,9 @@
 { config, lib, pkgs, ... }:
 
 {
-  users.users.homeassistant = {
-    isNormalUser = true;
-    group = "homeassistant";
-    extraGroups = [ "dialout" ];
-    home = "/var/lib/homeassistant";
-  };
-
-  users.groups.homeassistant = {};
+  systemd.tmpfiles.rules = [
+    "d /var/lib/homeassistant/config 0755 root root -"
+  ];
 
   systemd.services.homeassistant = {
     description = "Home Assistant";
@@ -19,8 +14,6 @@
     };
     serviceConfig = {
       Type = "forking";
-      User = "homeassistant";
-      Group = "homeassistant";
       ExecStartPre = "${pkgs.podman}/bin/podman rm -f homeassistant || true";
       ExecStart = "${pkgs.podman}/bin/podman run --detach --network host --device /dev/serial/by-id/usb-Itead_Sonoff_Zigbee_3.0_USB_Dongle_Plus_V2_5c3942dab64bef11ac6cc2a079f42d1b-if00-port0:/dev/ttyUSB0 -v /var/lib/homeassistant/config:/config --hostname homeassistant --name homeassistant ghcr.io/home-assistant/home-assistant:stable";
       ExecStop = "${pkgs.podman}/bin/podman stop homeassistant";
